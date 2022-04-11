991dcca715575d677fdfa555204b6034c55e0a83
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-04-11 18:53:23 -0300

    Coloca as tag's com os atributos do componente Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,16 @@ import React, { Component } from 'react';
 class Card extends Component {
   render() {
     return (
-      <div> </div>
+      <div>
+        <h3 data-testid="name-card"> </h3>
+        <img src="" alt="" data-testid="image-card" />
+        <p data-testid="description-card"> </p>
+        <p data-testid="attr1-card"> </p>
+        <p data-testid="attr2-card"> </p>
+        <p data-testid="attr3-card"> </p>
+        <p data-testid="rare-card"> </p>
+        <p data-testid="trunfo-card"> </p>
+      </div>
     );
   }
 }
