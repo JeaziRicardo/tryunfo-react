2c8aee57433a2fc6a7b91245e8e0bbd1c19959db
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-04-12 18:32:46 -0300

    Cria o estado inicial dos componentes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,22 @@ import Card from './components/Card';
 import Form from './components/Form';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: '',
+      cardAttr2: '',
+      cardAttr3: '',
+      cardImage: '',
+      cardRare: '',
+      cardTrunfo: false,
+      hasTrunfo: false,
+      isSaveButtonDisabled: false,
+    };
+  }
+
   render() {
     return (
       <div>
