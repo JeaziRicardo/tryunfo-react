f971743d7923b253463ac33392bc49859ea47f45
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-04-14 11:04:29 -0300

    Cria o componente CardList para exibir as cartas salvas

---
+++ src/components/CardList.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Card from './Card';
+
+class CardList extends Component {
+  render() {
+    const { cards } = this.props;
+    return (
+      <div>
+        {
+          cards.map((
+            {
+              cardName,
+              cardDescription,
+              cardAttr1,
+              cardAttr2,
+              cardAttr3,
+              cardImage,
+              cardRare,
+              cardTrunfo,
+            },
+          ) => (
+            <Card
+              key={ cardName }
+              cardName={ cardName }
+              cardDescription={ cardDescription }
+              cardAttr1={ cardAttr1 }
+              cardAttr2={ cardAttr2 }
+              cardAttr3={ cardAttr3 }
+              cardImage={ cardImage }
+              cardRare={ cardRare }
+              cardTrunfo={ cardTrunfo }
+            />
+          ))
+        }
+      </div>
+    );
+  }
+}
+
+CardList.propTypes = {
+  cards: PropTypes.arrayOf(Object).isRequired,
+};
+
+export default CardList;
